1b995c60697d0605edd9a280f186dbec9d3b0592
bharathappali <abharath@redhat.com>
2026-06-23 16:41:31 +0530

    add not null to enabled field
    Signed-off-by: bharathappali <abharath@redhat.com>

--- migrations/lm/v111__create_kruize_bulk_profile_table.sql
+++ migrations/lm/v111__create_kruize_bulk_profile_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS kruize_bulk_profile (
     clusters JSONB NOT NULL,
     recommendation_settings JSONB NOT NULL,
     webhook_url VARCHAR(500),
-    enabled BOOLEAN DEFAULT true,
+    enabled BOOLEAN NOT NULL DEFAULT true,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
